3e094c93472978b1be7d9698f7465fe8e77bbed4
resu <63846162+resu062@users.noreply.github.com>
2020-08-01 17:41:29 +0300

    app

--- li/layout-app/layout-app.js
+++ li/layout-app/layout-app.js
@@ -4,7 +4,8 @@ import '../button/button.js';
 class LiLayoutApp extends LitElement {
     static get properties() {
         return {
-            outside: { type: Boolean, reflect: true }, hide: { type: String },
+            outside: { type: Boolean, reflect: true }, sides: { type: String }, fill: { type: String },
+            hide: { type: String },
             widthL: { type: Number }, widthR: { type: Number },
             _move: { type: String }, _indx: { type: Number },
             _widthL: { type: Number }, _widthR: { type: Number },
@@ -13,48 +14,61 @@ class LiLayoutApp extends LitElement {
     }
     constructor() {
         super();
-        let prop = { // hide => 'lrtb' (left, right, top, bottom)
-            outside: false, hide: '', widthL: 300, widthR: 300,
+        let prop = { // hide => 'lrtb || LR' (left, right, top, bottom)
+            outside: false, hide: '', widthL: 300, widthR: 300, fill: '',
             _widthL: 300, _widthR: 300, _lastWidthL: 300, _lastWidthR: 300, _move: '', _indx: -1, _hl: '', _hr: ''
         }
         for (let i in prop) this[i] = prop[i];
+    }
+    connectedCallback() {
+        super.connectedCallback();
         if (this.hide.includes('l')) this._widthL = this.widthL = 0;
         if (this.hide.includes('r')) this._widthR = this.widthR = 0;
+        if (this.hide.includes('L')) this._hideL();
+        if (this.hide.includes('R')) this._hideR();
     }
     _movePanel(panel) {
         this._move = panel;
         this._indx = 999;
     }
     _up(e) {
+        let minSize = 100;
         e.preventDefault();
         this._indx = -1;
-        if (this._widthL >= document.body.clientWidth - this._widthR && this._move === 'left')
-            this._widthL = document.body.clientWidth - this._widthR - 200;
+        if (this._widthL >= document.body.clientWidth - this._widthR && this._move === 'left') 
+            this._widthL = document.body.clientWidth - this._widthR - minSize;
         else if (this._widthR >= document.body.clientWidth - this._widthL && this._move === 'right')
-            this._widthR = document.body.clientWidth - this._widthL - 200;
-        this._lastWidthL = this.widthL = this._widthL;
-        this._lastWidthR = this.widthR = this._widthR;
+            this._widthR = document.body.clientWidth - this._widthL - minSize;
+
+        this.widthL = this._widthL = this._widthL > minSize ? this._widthL : 0;
+        if (this.widthL === 0 && !this._hl) this._lastWidthL = minSize;
+        this._hl = this.widthL === 0 ? 'outl' : '';
+
+        this.widthR = this._widthR = this._widthR > minSize ? this._widthR : 0;
+        if (this.widthR === 0 && !this._hr) this._lastWidthR = minSize;
+        this._hr = this.widthR === 0 ? 'outr' : '';
+
         this._move = '';
         window.dispatchEvent(new Event('resize'));
     }
     _mousemove(e) {
         if (!this._move) return;
         e.preventDefault();
-        if (this._move === 'left') this._widthL = this._widthL + e.movementX;
-        if (this._move === 'right') this._widthR = this._widthR - e.movementX;
+        if (this._move === 'left') this.widthL = this._widthL = this._widthL + e.movementX;
+        if (this._move === 'right') this.widthR = this._widthR = this._widthR - e.movementX;
     }
     _hideL(e) {
         this._hl = this._hl ? '' : 'outl';
         let l = this._lastWidthL;
         this._lastWidthL = this.widthL;
-        this._widthL = this.widthL = (this.widthL > 34 ? 0 : l <= 34 ? 300 : l);
+        this._widthL = this.widthL = this._hl ? 0 : l;
         window.dispatchEvent(new Event('resize'));
     }
     _hideR(e) {
         this._hr = this._hr ? '' : 'outr';
         let r = this._lastWidthR;
         this._lastWidthR = this.widthR
-        this._widthR = this.widthR = (this.widthR > 34 ? 0 : r <= 34 ? 300 : r);
+        this._widthR = this.widthR = this._hr ? 0 : r;
         window.dispatchEvent(new Event('resize'));
     }
     static get styles() {
@@ -65,22 +79,20 @@ class LiLayoutApp extends LitElement {
             }
             .pnl-l-spl,
             .pnl-r-spl {
-                position: absolute;
-                top: 0; bottom: 0;
-                width: 6px;
+                max-width: 4px;
+                min-width: 4px;
                 cursor: col-resize;
-                z-index: 9;
             }
             .outl {
 	            transform: translateX(-100%);
             }
             .outr {
 	            transform: translateX(100%);
-            }       
+            } 
         `;
     }
 
-    get border() { 
+    get border() {
         return '1px solid lightgray'
     }
     get leftPanel() {
@@ -99,7 +111,7 @@ class LiLayoutApp extends LitElement {
     }
     get mainPanel() {
         return html`
-            <div style="flex: 1 1 auto; overflow: auto;width:100%">
+            <div style="flex: 1 1 auto; overflow: auto;width:100%; min-width: 200px">
                 <slot name="app-main"></slot>
             </div>
         `
@@ -107,9 +119,9 @@ class LiLayoutApp extends LitElement {
     get topPanel() {
         return this.hide.includes('t') ? '' : html`
             <div style="border-bottom: ${this.border}; display: flex; align-items: center; justify-content: center">
-                ${this.hide.includes('l') ? '' : html`<li-button size=28 br="none:50%" name="hamburger" fill="gray" toggled="left" style="padding:2px; cursor: pointer;" @click="${this._hideL}"></li-button>`}
+                ${this.hide.includes('l') ? '' : html`<li-button size=28 br="none:50%" name="hamburger" fill="${this.fill || 'gray'}" ?toggleded="${this._hl}" toggle="left" style="padding:2px; cursor: pointer;" @click="${this._hideL}"></li-button>`}
                 <div style="flex:1"><slot name="app-top"></slot></div>
-                ${this.hide.includes('r') ? '' : html`<li-button size=28 br="none:50%" name="hamburger" fill="gray" toggled="right" style="padding:2px; cursor: pointer;" @click="${this._hideR}"></li-button>`}
+                ${this.hide.includes('r') ? '' : html`<li-button size=28 br="none:50%" name="hamburger" fill="${this.fill || 'gray'}" ?toggleded="${this._hr}" toggle="right" style="padding:2px; cursor: pointer;" @click="${this._hideR}"></li-button>`}
             </div>
         `
     }
@@ -129,16 +141,18 @@ class LiLayoutApp extends LitElement {
     get tempPanel() {
         return html`<div class="temp" @mousemove="${this._mousemove}" @mouseup="${this._up}" style="z-index: ${this._indx}" @mouseout="${this._up}"></div>`
     }
-    get body() { 
+    get body() {
         return this.outside ?
             html`
                 <div style="display: flex; height: 100%;">
                     ${this.leftPanel}
-                    <div style="display: flex; flex-direction: column;overflow:hidden; flex:1">
+                    ${this.leftSplitter}
+                    <div style="display: flex; flex-direction: column;overflow:hidden; flex:1;">
                         ${this.topPanel}
                         ${this.mainPanel}
                         ${this.bottomPanel}
                     </div>
+                    ${this.rightSplitter}
                     ${this.rightPanel}
                 </div>
             ` :
@@ -147,7 +161,9 @@ class LiLayoutApp extends LitElement {
                     ${this.topPanel}
                     <div style="display: flex; flex: 1;overflow: hidden;">
                         ${this.leftPanel}
+                        ${this.leftSplitter}
                         ${this.mainPanel}
+                        ${this.rightSplitter}
                         ${this.rightPanel}
                     </div>
                     ${this.bottomPanel}
@@ -157,8 +173,6 @@ class LiLayoutApp extends LitElement {
     render() {
         return html`
             ${this.body}
-            ${this.leftSplitter}
-            ${this.rightSplitter}
             ${this.tempPanel}
         `;
     }
