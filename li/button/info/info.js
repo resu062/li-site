226e44bb56622823b3e030849c0118e2539c144c
resu <63846162+resu062@users.noreply.github.com>
2020-08-11 20:44:23 +0300

    misc

---
+++ li/button/info/info.js
@@ -0,0 +1,6 @@
+export const list = {
+    icon: ['name'],
+    //_icon: ['android', 'add', 'refresh', 'check'],
+    color: ['fill', 'color', 'back'],
+    _color: ['red', 'blue', 'green', 'orange', 'lightblue', 'lightgreen', 'lightyellow', 'yellow', 'dark', 'gray', 'darkgray', 'lightgray']
+}
