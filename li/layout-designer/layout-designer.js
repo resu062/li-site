7a3f88cded16865b1447d8a78cdb5a0171d3e7e6
resu <63846162+resu062@users.noreply.github.com>
2020-08-30 20:45:22 +0300

    designer

--- li/layout-designer/layout-designer.js
+++ li/layout-designer/layout-designer.js
@@ -10,6 +10,7 @@ class LayoutItem {
         this._keyID = props.keyID || 'id';
         this._keyLabel = props.keyLabel || 'label';
         this._keyItems = props.keyItems || 'items';
+        this._expanded = props.expanded || false;
     }
 
     get id() {
@@ -25,7 +26,7 @@ class LayoutItem {
         }
         return this._label;
     }
-    
+
     get items() {
         if (!this._items) {
             this._items = (this.$item[this._keyItems] || []).map(i => {
@@ -34,7 +35,17 @@ class LayoutItem {
         }
         return this._items;
     }
+
+    get $expanded() {
+        return this._expanded;
+    }
+
+    set $expanded(n) {
+        this._expanded = n;
+        LI.fire(document, 'updateTree', { ulid });
+    }
 }
+let ulid = LI.ulid();
 let dragInfo = {};
 
 customElements.define('li-layout-designer', class LiLayoutDesigner extends LiElement {
@@ -45,17 +56,13 @@ customElements.define('li-layout-designer', class LiLayoutDesigner extends LiEle
             keyID: { type: String, default: 'id' },
             keyLabel: { type: String, default: 'label' },
             keyItems: { type: String, default: 'items' },
-            designMode: { type: Boolean, default: false },
+            designMode: { type: String, default: 'false' }
         }
-    }
-
-    connectedCallback() {
-        super.connectedCallback();
     }
 
     updated(changedProps) {
         super.update(changedProps);
-        console.log(changedProps);
+        //console.log(changedProps);
         if (changedProps.has('item') && this.item) {
             this.layout = new LayoutItem(this.item, { keyID: this.keyID, keyLabel: this.keyLabel, keyItems: this.keyItems });
         }
@@ -65,11 +72,131 @@ customElements.define('li-layout-designer', class LiLayoutDesigner extends LiEle
         return html`
             <li-layout-app hide="r">
                 <div slot="app-top">li-layout-designer</div>
-                <li-button slot="app-top-right" name="tree-structure" toggledClass="ontoggled" style="margin-right: 4px;"></li-button>>
+                <li-button slot="app-top-right" name="tree-structure" toggledClass="ontoggled" style="margin-right: 4px;" @click="${this._setDesignMode}"></li-button>>
                 <div slot="app-left" style="margin:4px 0px 4px 4px; border: .5px solid lightgray;border-bottom:none">
-                    <li-tree .item="${this.layout}"></li-tree>
+                    <li-tree .item="${this.layout}" ulid="${ulid}"></li-tree>
                 </div>
+                <li-layout-structure id="structure" slot="app-main" .items="${this.layout && this.layout.items || this.layout}" designMode="${this.designMode}"></li-layout-structure>
             </li-layout-app>
         `;
+    }
+
+    _setDesignMode(e) {
+        this.designMode = e.target.toggled;
+    }
+});
+
+customElements.define('li-layout-structure', class LiLayoutStructure extends LiElement {
+    static get properties() {
+        return {
+            items: { type: Array, default: [] },
+            designMode: { type: String, default: 'false' }
+        }
+    }
+
+    render() {
+        if (!this.items || !this.items.map) return html``;
+        return html`
+            <style>
+                :host {
+                    border: .5px solid lightblue;
+                    margin: 2px;
+                    padding: 2px;
+                    overflow: auto;
+                    display: flex;
+                    flex-direction: column;
+                }
+            </style>
+            ${this.items.map(i => html`
+                <li-layout-container .item=${i} designMode="${this.designMode}"></li-layout-container>
+            `)}
+        `;
+    }
+});
+
+customElements.define('li-layout-container', class LiLayoutContainer extends LiElement {
+    static get properties() {
+        return {
+            item: { type: Object, default: {} },
+            designMode: { type: String, default: 'false' },
+            iconSize: { type: Number, default: 24 }
+        }
+    }
+
+    constructor() {
+        super();
+        this.__requestUpdate = this._requestUpdate.bind(this);
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        LI.listen(document, 'updateTree', this.__requestUpdate);
+    }
+    disconnectedCallback() {
+        LI.unlisten(document, 'updateTree', this.__requestUpdate);
+        super.disconnectedCallback();
+    }
+
+    _requestUpdate(e) {
+        if (e.detail && e.detail.ulid === ulid) this.requestUpdate();
+    }
+
+    render() {
+        return html`
+            <style>
+                :host {
+                    min-width: 200px;
+                }
+                .complex {
+                    margin-left: 16px;
+                    overflow: hidden;
+                }
+                oda-icon {
+                    cursor: pointer;
+                }
+                .design-row {
+                    border: .5px dotted lightgray;
+                    cursor: move;
+                }
+                .design-row:hover {
+                    background: lightyellow;
+                }
+            </style>
+            <div class="${this.designMode ? 'design-row' : ''}" :draggable @dragstart="${this._dragstart}" @dragend="${this._dragend}"
+                    @dragover="${this._dragover}" @dragleave="${this._dragleave}" @drop="${this._dragdrop}"  style="display:flex;align-items:center">
+                ${this.item && this.item.items && this.item.items.length ? html`
+                    <li-button name="chevron-right" toggledClass="right90" toggle="${this.item && this.item.$expanded}" style="pointer-events:visible" @click="${this._toggleExpand}" border="0"></li-button>`
+                : html`
+                    <div style="width:${this.iconSize}px;height:${this.iconSize}px;"></div>
+                `}
+                <label style="cursor: move;">${this.item && this.item.label}</label>
+                <div style="flex:1;"></div>
+            </div>
+            ${this.item && this.item.items && this.item.items.length && this.item.$expanded ? html`
+                <li-layout-structure class="complex"
+                    .items="${this.item.items || []}" designMode="${this.designMode}" style="flex-wrap: wrap;"></li-layout-structure>` : ''
+            }
+        
+        `;
+    }
+
+    _toggleExpand(e) {
+        this.item.$expanded = e.target.toggled;
+        this.requestUpdate();
+    }
+    _dragstart() {
+
+    }
+    _dragend() {
+
+    }
+    _dragover() {
+
+    }
+    _dragleave() {
+
+    }
+    _dragdrop() {
+
     }
 });
--- li/tree/tree.js
+++ li/tree/tree.js
@@ -5,6 +5,7 @@ import '../button/button.js';
 customElements.define('li-tree', class LiTree extends LiElement {
     static get properties() {
         return {
+            ulid: { type: String, default: '' },
             item: { type: Object, default: {} },
             iconSize: { type: String, default: '24' },
             margin: { type: String, default: '0' },
@@ -13,20 +14,39 @@ customElements.define('li-tree', class LiTree extends LiElement {
         }
     }
 
+    constructor() {
+        super();
+        this.__requestUpdate = this._requestUpdate.bind(this);
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        LI.listen(document, 'updateTree', this.__requestUpdate);
+    }
+    disconnectedCallback() {
+        LI.unlisten(document, 'updateTree', this.__requestUpdate);
+        super.disconnectedCallback();
+    }
+
+    _requestUpdate(e) {
+        if (e.detail && e.detail.ulid === this.ulid) this.requestUpdate();
+    }
+
     render() {
+        if (!this.item || (!this.item.items && !this.item.map)) return html``;
         return html`
-            ${(this.item.items || this.item).map(i => html`
+            ${(this.item.items || this.item && this.item).map(i => html`
                 <div style="${this.fullBorder ? 'border-bottom: .5px solid ' + this.colorBorder : ''}">
                     <div style="display:flex;align-items:center;margin-left:${this.margin}px;${!this.fullBorder ? 'border-bottom: .5px solid ' + this.colorBorder : ''}">
                         ${i.items && i.items.length
                             ? html`<li-button name="chevron-right" border="0" toggledClass="right90" toggle="${i.$expanded}"
-                                            @click="${(e) => this._click(e, i)}" size="${this.iconSize}"></li-button>`
+                                    @click="${(e) => this._click(e, i)}" size="${this.iconSize}"></li-button>`
                             : html`<div style="min-width:${this.iconSize}px;width:${this.iconSize}px;min-height:${this.iconSize}px;height:${this.iconSize}px"></div>`
                         }
                         <div style="padding:2px">${i.label || i.name}</div>
                     </div>
                 </div>
-                ${i.items && i.items.length && i.$expanded ? html`<li-tree .item="${i.items}" margin="${Number(this.margin) + Number(this.iconSize)}"></li-tree>` : ''}
+                ${i.items && i.items.length && i.$expanded ? html`<li-tree .item="${i.items}" margin="${Number(this.margin) + Number(this.iconSize)}" ulid="${this.ulid}"></li-tree>` : ''}
             `)}
         `
     }
