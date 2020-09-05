4946e17ff261647a8cc6921712534732bd8f1c5e
resu <63846162+resu062@users.noreply.github.com>
2020-09-05 12:28:23 +0300

    layout-designer

--- li/tree/tree.js
+++ li/tree/tree.js
@@ -7,10 +7,11 @@ customElements.define('li-tree', class LiTree extends LiElement {
         return {
             ulid: { type: String, default: '' },
             item: { type: Object, default: {} },
-            iconSize: { type: String, default: '24' },
+            iconSize: { type: String, default: '28' },
             margin: { type: String, default: '0' },
             fullBorder: { type: Boolean, default: false },
-            colorBorder: { type: String, default: 'lightgray' }
+            colorBorder: { type: String, default: 'lightgray' },
+            verticalLine: { type: Boolean, default: true }
         }
     }
 
@@ -32,21 +33,35 @@ customElements.define('li-tree', class LiTree extends LiElement {
         if (e.detail && e.detail.ulid === this.ulid) this.requestUpdate();
     }
 
+    static get styles() {
+        return css`
+            .complex-line {
+                border-left: 1px dashed lightgray;
+            }
+            .complex {
+                margin-left: 14px;
+                overflow: hidden;
+            }
+        `;
+    }
+
     render() {
         if (!this.item || (!this.item.items && !this.item.map)) return html``;
         return html`
             ${(this.item.items || this.item && this.item).map(i => html`
                 <div style="${this.fullBorder ? 'border-bottom: .5px solid ' + this.colorBorder : ''}">
-                    <div style="display:flex;align-items:center;margin-left:${this.margin}px;${!this.fullBorder ? 'border-bottom: .5px solid ' + this.colorBorder : ''}">
+                    <div style="display:flex;align-items:center;margin-left:${this.margin}px;${!this.fullBorder ? 'border-bottom: 1px solid ' + this.colorBorder : ''}">
                         ${i.items && i.items.length
-                            ? html`<li-button name="chevron-right" border="0" toggledClass="right90" ?toggled="${i.$expanded}"
+                ? html`<li-button back="transparent" name="chevron-right" border="0" toggledClass="right90" ?toggled="${i.$expanded}"
                                     @click="${(e) => this._click(e, i)}" size="${this.iconSize}"></li-button>`
-                            : html`<div style="min-width:${this.iconSize}px;width:${this.iconSize}px;min-height:${this.iconSize}px;height:${this.iconSize}px"></div>`
-                        }
+                : html`<div style="min-width:${this.iconSize}px;width:${this.iconSize}px;min-height:${this.iconSize}px;height:${this.iconSize}px"></div>`
+            }
                         <div style="padding:2px">${i.label || i.name}</div>
                     </div>
                 </div>
-                ${i.items && i.items.length && i.$expanded ? html`<li-tree .item="${i.items}" margin="${Number(this.margin) + Number(this.iconSize)}" ulid="${this.ulid}"></li-tree>` : ''}
+                <div class="complex ${this.verticalLine ? 'complex-line' : ''}">
+                    ${i.items && i.items.length && i.$expanded ? html`<li-tree .item="${i.items}" margin="${Number(this.margin)}" ulid="${this.ulid}"></li-tree>` : ''}
+                </div>
             `)}
         `
     }
