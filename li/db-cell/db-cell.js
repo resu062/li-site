c38f8ec185bea152f2496753e373970bab4ee3de
resu <63846162+resu062@users.noreply.github.com>
2020-10-19 22:25:25 +0300

    tabs

--- li/db-cell/db-cell.js
+++ li/db-cell/db-cell.js
@@ -5,13 +5,16 @@ import '../button/button.js';
 customElements.define('li-db-cell', class LiDbCell extends LiElement {
     static get properties() {
         return {
+            item: { type: Object, default: undefined },
             icon: { type: String, default: 'apps' },
             label: { type: String, default: 'db-cell ...' },
             action: { type: String, default: '' },
             callback: { type: Object, default: undefined },
             iconOpen: { type: String, default: 'flip-to-front' },
             iconSettings: { type: String, default: 'settings' },
-            hideIcons: { type: String, default: '' }
+            hideIcons: { type: String, default: '' },
+            width: { type: String, default: '260px' },
+            liSize: { type: Number, default: 28, local: true }
         }
     }
 
@@ -26,32 +29,38 @@ customElements.define('li-db-cell', class LiDbCell extends LiElement {
 
     static get styles() {
         return css`
-                .db-cell {
-                    display:flex;
-                    align-items: center;
-                    justify-content: stretch;
-                    border: 1px solid lightgray;
-                    background: whitesmoke;
-                    width: 260px;
-                }
+            .db-cell {
+                display:flex;
+                align-items: center;
+                justify-content: stretch;
+                border: 1px solid lightgray;
+                background: whitesmoke;
+            }
+            .label {
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
         `;
     }
 
     render() {
         return html`
-            <div class="db-cell" @click="${this._tap}">
-                <li-button name="${this.icon}" label="${this.label}" width="auto" style="flex:1" textAlign="left" borderColor="lightgray" border="0"></li-button>
+            <div class="db-cell" @click="${this._tap}" style="width: ${this.width};">
+                <li-button id="btn1" class="label" name="${this.icon}" label="${this.label}" width="auto" style="flex:1;" textAlign="left" borderColor="lightgray" border="0" size="${this.liSize}"></li-button>
                 ${!this.iconOpen ? html`` : html`
-                    <li-button name="${this.iconOpen}" fill="lightgray" border="0"></li-button>
+                    <li-button id="btn2" name="${this.iconOpen}" fill="lightgray" border="0"></li-button>
                 `}
                 ${!this.iconSettings ? html`` : html`
-                    <li-button name="${this.iconSettings}" fill="lightgray" border="0"></li-button>
+                    <li-button id="btn3" name="${this.iconSettings}" fill="lightgray" border="0"></li-button>
                 `}
             </div>
         `;
     }
 
     async _tap(e) {
-        LI.fire(document, "dropdownDataChange", { target: this, value: e.target.label || e.target.name, callback: this.callback, action: this.action });
+        //e.stopPropagation();
+        this._target = e.target;
+        LI.fire(document, "dropdownDataChange", { id: e.target.id, target: this, value: e.target.label || e.target.name, callback: this.callback, action: this.action });
     }
 });
---
+++ li/tabs/tabs.js
@@ -0,0 +1,90 @@
+import { html, css } from '../../lib/lit-element/lit-element.js';
+import { LiElement } from '../../li.js';
+import '../db-cell/db-cell.js';
+
+customElements.define('li-tabs', class LiTabs extends LiElement {
+    static get properties() {
+        return {
+            $$id: { type: String, default: '' },
+            item: { type: Object, default: {} },
+            liSize: { type: Number, default: 28, local: true },
+            vertical: { type: Boolean, default: false },
+            horizontal: { type: Boolean, default: false },
+            iconSettingsV: { type: String, default: 'more-vert' },
+            iconSettingsH: { type: String, default: 'more-horiz' }
+        }
+    }
+
+    get items() {
+        return this.item && this.item.map ? this.item : this.item && this.item.items && this.item.items.map ? this.item.items : [];
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                margin: 2px;
+            }
+            .tabs {
+                display: flex;
+            }
+            .cell {
+                margin: 2px;
+            }
+        `;
+    }
+
+    render() {
+        return html`
+            <div class="buttons" style="display:flex;flex-direction:${this.horizontal ? 'row' : 'column'};position:sticky;top:0px;float:right;z-index:9;">
+                <li-button name="chevron-left" size="${this.liSize}" style="margin:2px;"></li-button> 
+                <li-button name="chevron-right" size="${this.liSize}" style="margin:2px;"></li-button>  
+                <li-button name="expand-more" size="${this.liSize}" style="margin:2px;"></li-button>
+                <li-button name="settings" size="${this.liSize}" style="margin:2px;" @click="${this._clickSettings}"></li-button>
+            </div> 
+            <div class="tabs" style="overflow:auto;flex-direction:${this.vertical ? 'column' : 'row'}">
+                ${this.items.map(i => html`
+                    <li-db-cell class="cell" .item="${i}" icon="${i.icon}" label="${i.label}" hideIcons="2" iconSettings="close" liSize="${this.liSize}" @click="${this._clickCell}"></li-db-cell>    
+                `)}
+            </div>
+        `
+    }
+    async _clickSettings(e) {
+        this._iconSettings = this._iconSettings === this.iconSettingsH ? this.iconSettingsV : this.iconSettingsH;
+        let val = await LI.show('dropdown', 'db-cell-list', {
+            list: [
+                { icon: this._iconSettings, label: 'Rotate menu', action: 'rotateMenu', hideIcons: '23' },
+                { icon: 'repeat', label: 'Rotate tabs', action: 'rotateTabs', hideIcons: '23' },
+                { icon: 'add', label: 'Add tab', action: 'addTab', hideIcons: '23' },
+            ]
+        }, {});
+        switch (val.detail.action) {
+            case 'addTab':
+                if (this.item && this.item.map) {
+                    this.item.push({ label: 'new-tab' });
+                } else if (this.item && this.item.items && this.item.items.map) {
+                    this.item.items.push({ label: 'new-tab' });
+                }
+                break;
+            case 'rotateTabs':
+                this.vertical = !this.vertical;
+                break;
+            case 'rotateMenu':
+                this.horizontal = !this.horizontal;
+                break;
+            default:
+                break;
+        }
+        this.$$update();
+    }
+    _clickCell(e) {
+        if (e.target._target && e.target._target.id === 'btn3') {
+            console.log(e);
+            if (this.item && this.item.map) {
+                this.item.splice(this.item.indexOf(e.target.item), 1);
+            } else if (this.item && this.item.items && this.item.items.map) {
+                this.item.items.splice(this.item.items.indexOf(e.target.item), 1);
+            }
+        }
+        this.$$update();
+    }
+});
