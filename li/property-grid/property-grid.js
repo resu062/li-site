408a942a095230070fc95d71b5406ceb330295f0
resu <63846162+resu062@users.noreply.github.com>
2021-02-17 06:41:30 +0300

    Update property-grid.js

--- li/property-grid/property-grid.js
+++ li/property-grid/property-grid.js
@@ -1,19 +1,22 @@
 import { html, css } from '../../lib/lit-element/lit-element.js';
 import { LiElement } from '../../li.js';
-// import { LItem, ldfn } from './li-data.js';
 import '../button/button.js';
-import '../checkbox/checkbox.js';
 
 customElements.define('li-property-grid', class LiPropertyGrid extends LiElement {
     static get properties() {
         return {
             _$$id: { type: String, default: '', update: true },
-            _inspectedObject: { type: Object, default: undefined },
+            inspectedObject: { type: Object, default: undefined },
+            ioProperties: { type: Object, default: {} },
             expertMode: { type: Boolean, default: false, local: true },
-            litem: { type: Object, default: undefined },
-            props: { type: Object, default: {} },
+            item: { type: Object, default: undefined },
             iconSize: { type: String, default: '28', local: true },
-            labelColumn: { type: Number, default: 250, local: true }
+            sort: { type: String, default: 'none', local: true },
+            labelColumn: { type: Number, default: 250, local: true },
+            testOBJ: { type: Object, default: { 
+                obj1: { name: 'test Object1', arr: [{ prop1: 1 }, { prop2: 2 }, { prop3: 3, arr: [{ prop1: 1 }, { prop2: 2 }, { prop3: 3, arr: [{ prop1: 1 }, { prop2: 2 }, { prop3: 3 }] }] }], description: 'note...' },
+                obj2: { name: 'test Object2', arr: [{ prop1: 1 }, { prop2: 2 }, { prop3: 3, arr: [{ prop1: 1 }, { prop2: 2 }, { prop3: 3, arr: [{ prop1: 1 }, { prop2: 2 }, { prop3: 3 }] }] }], description: 'note...' } },
+            }
         }
     }
 
@@ -23,84 +26,92 @@ customElements.define('li-property-grid', class LiPropertyGrid extends LiElement
     }
 
     getData() {
-        if (!this.litem) {
-            this._inspectedObject = window//this.$id.btn;
-            this.litem = makeData(this._inspectedObject, this.expertMode);
-            // this.litem = new LItem(data, this.props, undefined, undefined, this.$$id);
-            // this.litem.$root = this.litem;
-        }
+        this.inspectedObject = this;
+        this.item = makeData(this.inspectedObject, this.expertMode);
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                display: flex;
+                flex-direction: column;
+            }
+            .header {
+                position: sticky;
+                top: 0px;
+                display: flex;
+                background-color: gray;
+            }
+            .label {
+                display: flex;
+                flex: 1;
+                align-items: center;
+                justify-content: center;
+                font-size: large;
+                color: white;
+            }
+            .buttons {
+                display: flex;
+                padding: 2px;
+            }
+            .container {
+                flex: 1;
+                overflow-y: auto;
+                overflow-x: hidden;
+                /* border:1px solid red; */
+            }            
+            .group-header {
+                position: sticky;
+                top: 0px;
+                display: flex;
+                align-items: center;
+                background-color: #f0f0f0;
+                border-bottom:1px solid gray;
+                min-height: 28px;
+                padding-left: 16px;
+            }
+        `;
     }
 
     render() {
         return html`
-            <div style="border:1px solid gray;overflow-y:auto;overflow-x:hidden;display:flex;flex-direction:column;height:100%">
-                <div style="font-size:large;color:gray;z-index:1;min-height:32px;position:sticky;top:0px;display:flex;align-items:center;justify-content: center;width:100%;border-bottom:1px solid gray;height:32px;background:lightgray">${this.litem?.label || 'PropertyGrid'}</div>
-                <li-button id="btn" toggledClass="ontoggled" name="settings" title="expertMode" style="z-index:1;position:absolute;right:2px;top:3px" @click="${(e) => this._expert(e)}" ></li-button>
-                <li-property-tree .litem="${this.litem}" .$$id="${this.$$id}"></li-property-tree>
+            <div class="header">
+                <div class="label">${this.item?.label || 'PropertyGrid'}</div>
+                <div class="buttons" >
+                    <li-button id="btn" name="refresh" title="refresh" @click="${(e) => this._expert()}"></li-button>
+                    <li-button id="btn" name="sort" title="sort" @click="${(e) => this._sort()}"></li-button>
+                    <li-button id="btn" toggledClass="ontoggled" name="list" title="group"></li-button>
+                    <li-button id="btn" toggledClass="ontoggled" name="settings" title="expertMode" @click="${(e) => this._expert(e)}"></li-button>
+                </div>
+            </div>
+            <div class="container">
+                <div class="group">
+                    <div class="group-header">properties</div>
+                    <li-property-tree class="tree" .item="${this.item}" .$$id="${this.$$id}"></li-property-tree>
+                    <div class="group-header">no category</div>
+                    <li-property-tree .item="${this.item}" .$$id="${this.$$id}"></li-property-tree>
+                </div>
             </div>
         `
     }
     _expert(e) {
-        this.litem = undefined;
-        this.expertMode = e.target.toggled;
+        this.expertMode = e?.target?.toggled || e ? e?.target?.toggled : this.expertMode;
         this.getData();
-        this.$$update();
+        //this.$$update();
+    }
+    _sort(e) {
+        this.sort = this.sort === 'none' ? 'ascending' : this.sort === 'ascending' ? 'descending' : 'none';
+        this.getData();
+        //this.$$update();
     }
 });
-
-const exts = /^(_|\$)/;
-function makeData(el, expert) {
-    const data = { label: el?.localName, items: [] };
-    const props = el.constructor._classProperties;
-    if (props) {
-        for (const key of props.keys()) {
-            if (!expert && exts.test(key)) continue;
-            let value = el[key],
-                is = false
-            if (value != null && value.constructor.name === "Array") {
-                value = 'array [' + value.length + ']';
-                is = true;//el[key].props || el[key].properties || expert || false;
-            }
-            else if (value != null && (value.constructor.name === "Object" || value.toString().includes('object'))) {
-                value = '[Object]';
-                is = true;//el[key].props || el[key].properties || expert || false;
-            }
-
-            data.items.push({ label: key, value, is, el: el[key], items: [] });
-        }
-    }
-    let obj = el;
-    while (obj) {
-        let names = Object.getOwnPropertyNames(obj);
-        for (let key of names) {
-            if (!expert && exts.test(key)) continue;
-            if (/^(__|props|properties)/.test(key)) continue;
-            const d = Object.getOwnPropertyDescriptor(obj, key);
-            if (!d || typeof d.value === 'function') continue;
-            let value = el[key],
-                is = false
-            if (value != null && value.constructor.name === "Array") {
-                value = 'Array [' + value.length + ']';
-                is = true;//el[key].props || el[key].properties || expert || false;
-            }
-            else if (value != null && (value.constructor.name === "Object" || value.toString().includes('object'))) {
-                value = '[Object]';
-                is = expert || el[key].props || el[key].properties || false;
-            }
-            data.items.push({ label: key, value, is, el: el[key], items: [] });
-        }
-        if (!expert) break;
-        obj = obj.__proto__;
-    }
-    return data;
-}
 
 customElements.define('li-property-tree', class LiPropertyTree extends LiElement {
     static get properties() {
         return {
             $$id: { type: String, default: '', update: true },
             expertMode: { type: Boolean, default: false, local: true },
-            litem: { type: Object, default: undefined },
+            item: { type: Object, default: undefined },
             props: { type: Object, default: {} },
             iconSize: { type: String, default: '28', local: true },
             labelColumn: { type: Number, default: 250, local: true }
@@ -108,7 +119,7 @@ customElements.define('li-property-tree', class LiPropertyTree extends LiElement
     }
 
     get _items() {
-        return this.litem && this.litem.map ? this.litem : this.litem && this.litem.items && this.litem.items.map ? this.litem.items : [];
+        return this.item && this.item.map ? this.item : this.item && this.item.items && this.item.items.map ? this.item.items : [];
     }
 
     static get styles() {
@@ -137,6 +148,14 @@ customElements.define('li-property-tree', class LiPropertyTree extends LiElement
                 flex: 1;
                 min-width: 0px;
             }
+            .label {
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                overflow: hidden;
+                display: flex;
+                padding:0 2px;
+                align-items: center;
+            }
         `;
     }
 
@@ -149,12 +168,12 @@ customElements.define('li-property-tree', class LiPropertyTree extends LiElement
                 ? html`<li-button back="transparent" name="chevron-right" border="0" toggledClass="right90" .toggled="${i.expanded}"
                                     @click="${(e) => this._expanded(e, i)}" size="${this.iconSize - 2}"></li-button>`
                 : html`<div style="min-width:${this.iconSize}px;width:${this.iconSize}px;min-height:${this.iconSize}px;height:${this.iconSize}px"></div>`}
-                        <div style="overflow:hidden;display:flex;padding:0 2px;max-width:${this.labelColumn}px;min-width:${this.labelColumn}px;white-space: nowrap;height:${this.iconSize}px;align-items: center;">${i.label || i.name}</div>
+                        <div class="label" style="max-width:${this.labelColumn}px;min-width:${this.labelColumn}px;height:${this.iconSize}px;">${i.label || i.name}</div>
                         <input id="input" value="${i.value}" style="display:flex;padding:0 2px;white-space: nowrap;border-left:1px solid lightgray;height:${this.iconSize}px;align-items: center;">
                     </div>
                 </div>
                 <div class="complex">
-                    ${(i.el || i.items.length) && i.expanded ? html`<li-property-tree .litem="${i.data}" .$$id="${this.$$id}"></li-property-tree>` : html``}
+                    ${(i.el || i.items.length) && i.expanded ? html`<li-property-tree .item="${i.data}" .$$id="${this.$$id}"></li-property-tree>` : html``}
                 </div>
             `)}
         `
@@ -172,3 +191,46 @@ customElements.define('li-property-tree', class LiPropertyTree extends LiElement
         // this.$$update();
     }
 });
+
+function makeData(el, expert) {
+    const fn = (key) => {
+        let value = el[key],
+            is = false
+        if (value && Array.isArray(value)) {
+            value = 'Array [' + value.length + ']';
+            is = true;
+        }
+        else if (value !== null && typeof value === 'object') {
+            value = '[Object]';
+            is = true;
+        }
+        data.items.push({ label: key, value, is, el: el[key], items: [] });
+    }
+
+    const exts = /^(_|\$)/;
+    const data = { label: el?.localName, items: [] };
+    const props = el.constructor._classProperties;
+
+    if (props) {
+        for (const key of props.keys()) {
+            if (!expert && exts.test(key)) continue;
+            fn(key);
+        }
+    }
+
+    let obj = el;
+    while (obj) {
+        let names = Object.getOwnPropertyNames(obj);
+        for (let key of names) {
+            if (!expert && exts.test(key)) continue;
+            if (/^(__|props|properties|$props)/.test(key)) continue;
+            const d = Object.getOwnPropertyDescriptor(obj, key);
+            if (!d || typeof d.value === 'function') continue;
+            fn(key);
+        }
+        if (!expert) break;
+        obj = obj.__proto__;
+    }
+
+    return data;
+}
